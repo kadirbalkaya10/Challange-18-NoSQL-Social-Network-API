812c6edd2915cb03aa9c8cba0b7bb8b5589ba726
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-05-07 00:23:39 -0400

    User model, added virtual option, added comments to explain each line of code

--- models/User.js
+++ models/User.js
@@ -1,42 +1,57 @@
 //Dependencies
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose"); // Importing mongoose library
+const Schema = mongoose.Schema; // Extracting Schema class from mongoose
 
 // Email Validation with Regular expression
 var validateEmail = function (email) {
-  var reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return reg.test(email);
+  var reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // Regular expression for email validation
+  return reg.test(email); // Checking if email matches the regular expression
 };
 
 // Set up User Collection Data Structure
-const userScheme = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: "Email address is required",
-    unique: true,
-    trim: true,
-    lowercase: true,
-    validate: [validateEmail, "Please fill a valid email address"],
-  },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Thought",
+const userScheme = new mongoose.Schema(
+  {
+    username: {
+      // Defining username field
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-  ],
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
+    email: {
+      // Defining email field
+      type: String,
+      required: "Email address is required",
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate: [validateEmail, "Please fill a valid email address"],
     },
-  ],
-});
+    // Defining thoughts field as an array of ObjectIds referencing the Thought model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId, // Field type is ObjectId
+        ref: "Thought", // Referencing the Thought model
+      },
+    ],
+    // Defining friends field as an array of ObjectIds referencing the User model
+    friends: [
+      {
+        type: Schema.Types.ObjectId, // Field type is ObjectId
+        ref: "User", // Referencing the User model
+      },
+    ],
+  },
+  {
+    // Configuring toJSON option
+    toJSON: {
+      // Including virtual properties when converting document to JSON
+      virtuals: true,
+    },
+    // Disabling generation of default '_id' field
+    id: false,
+  }
+);
 
 // Virtual to retrieve friendCount
 userScheme.virtual("friendCount").get(function () {
